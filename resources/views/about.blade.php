6c4a11e715215c90138fc6793e87f2e8580fa42f
Aphidet H <Aphidet.h@abctech.co.th>
2019-11-06 16:13:48 +0700

    Lession 1

---
+++ resources/views/about.blade.php
@@ -0,0 +1,9 @@
+@extends('layout')
+@section('title','About Us')
+  
+@section('content')
+    <h1>About us.</h1>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris semper cursus magna id auctor. Proin sit amet elit velit. Etiam interdum, turpis vitae tincidunt rhoncus, lacus enim fringilla arcu, sed blandit tortor nibh et turpis. Morbi aliquam et nulla et dictum. Duis et ligula elementum felis laoreet congue. Cras venenatis a nisi non porta. Sed tincidunt vel augue at auctor. Cras cursus odio et fringilla volutpat. Integer blandit leo lacus, ut viverra felis sodales ac. Vivamus semper dolor feugiat mi laoreet, ac suscipit ligula convallis. Etiam nec pulvinar lorem. Cras dignissim neque eget tincidunt faucibus. Curabitur ac turpis bibendum, tincidunt dolor ut, eleifend tortor.
+    </p>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,22 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
-    return view('welcome');
+    $tasks = [
+        'Go to the store',
+        'Go to the market',
+        'Go to the home',
+    ];
+    return view('welcome', [
+        'tasks' => $tasks,
+    ]);
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/contact', function () {
+    return view('contact');
 });
